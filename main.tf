ee09b4853be89577f043e741dc7935442e926d61
Brodie <36842637+brodiemackenzie@users.noreply.github.com>
2021-04-08 17:25:40 +0000

    add HC API dataset and HL7v2 store

--- main.tf
+++ main.tf
@@ -16,6 +16,15 @@ provider "google" {
 
 }
 
+provider "google-beta" {
+
+  credentials = file(var.credentials_file)
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+
+}
+
 # Enable services
 # Create notebook service account 
 # Add roles to notebook service account
@@ -30,14 +39,43 @@ resource "random_string" "bucket" {
 resource "google_storage_bucket" "sample_bucket" {
   name     = "hcdh-sample-data-${random_string.bucket.result}"
   location = "US"
+  force_destroy = true
+
+# Upload sample data to GCS bucket
 
   provisioner "local-exec" {
     command = "gsutil -m cp -r ~/healthcare-data-harmonization/mapping_configs/* ${google_storage_bucket.sample_bucket.url}/mapping_configs/"
   }
 }
 
-# Upload sample data to GCS bucket
 # Create Healthcare API Dataset
+
+resource "google_healthcare_dataset" "dataset" {
+  provider = google-beta
+  name     = "hcdh-dataset"
+  location = "us-central1"
+}
+
 # Create HL7v2 Store
+
+resource "google_healthcare_hl7_v2_store" "store" {
+  provider = google-beta
+  name    = "hcdh-hl7-v2-store"
+  dataset = google_healthcare_dataset.dataset.id
+
+  parser_config {
+    
+    allow_null_header  = false
+    segment_terminator = "Cg=="
+    version            = "V2"
+    schema = <<EOF
+    {
+        "schematizedParsingType": "SOFT_FAIL",
+        "ignoreMinOccurs": "true"
+    }
+    EOF    
+    }
+}
+
 # Upload sample message to HL7v2 store 
-# Create .env variables file 
+# Create .env variables file
